7785c257256a7b8f5af4da9ba3dc062da00698f8
Joel Berg <bergjoel93@gmail.com>
2024-11-21 11:03:58 -0700

    railway testing

--- db/db.js
+++ db/db.js
@@ -1,6 +1,12 @@
 // db/db.js
 require("dotenv").config();
 const { Pool } = require("pg");
+console.log("Database_Url", process.env.DATABASE_URL);
+
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL is not set");
+  process.exit(1); // Exit the process if the database URL is missing
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
